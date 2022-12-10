885eaeb3f66df76029e5d4030ae1ae3b8a20a23c
Lee <leejavaa@gmail.com>
2022-12-10 20:02:19 +0200

    MIK-13-Implemented basic slider

--- data/projects.js
+++ data/projects.js
@@ -7,13 +7,13 @@ const projects = [
     about:
       'The Eden Experience is an internationally recognised dance music festival brand that hails from Cape Town, South Africa. In February 2019, Mikhail undertook the design and installation of the main stage, which naturally found itself at the center of the experience.',
     quote: 'Mikhail says something here',
-    display_image: '../static/images/eden-feb/display.png',
+    display_image: '/static/images/eden-feb/display.png',
     display_caption: 'Main Stage',
-    carousel_image: '../static/images/eden-feb/carousel.png',
+    carousel_image: '/static/images/eden-feb/carousel.png',
     gallery: [
-      '../static/images/eden-feb/gallery1.png',
-      '../static/images/eden-feb/gallery2.png',
-      '../static/images/eden-feb/gallery3.png',
+      '/static/images/eden-feb/gallery1.png',
+      '/static/images/eden-feb/gallery2.png',
+      '/static/images/eden-feb/gallery3.png',
     ],
   },
   {
@@ -24,13 +24,13 @@ const projects = [
     about:
       'The Eden Experience is an internationally recognised dance music festival brand that hails from Cape Town, South Africa. This edition took place during November 2019, with Mikhail asked to design and lead the installation of the main stage once more, and for the second time running, the crowd was not disappointed.',
     quote: 'Mikhail says something here',
-    display_image: '../static/images/eden-nov/display.png',
+    display_image: '/static/images/eden-nov/display.png',
     display_caption: 'main stage',
-    carousel_image: '../static/images/eden-nov/carousel.png',
+    carousel_image: '/static/images/eden-nov/carousel.png',
     gallery: [
-      '../static/images/eden-nov/gallery1.png',
-      '../static/images/eden-nov/gallery2.png',
-      '../static/images/eden-nov/gallery3.png',
+      '/static/images/eden-nov/gallery1.png',
+      '/static/images/eden-nov/gallery2.png',
+      '/static/images/eden-nov/gallery3.png',
     ],
   },
   {
@@ -42,13 +42,13 @@ const projects = [
       "\"We've transported you to whirling galaxies but our Toybox is still bursting at the seams... don't let gravity suck you in; we're slaying dragons and we're taking you with us... are you ready player one?\"\nToybox is a dance music festival which brings in the best of both local and international artist to peform in front of thousands of festival goers. In this edition, Mikhail led the way with the set design, fabrication and assembly, creating one of Toybox's most memorable stages to date.",
     quote:
       'I wanted build a giant oldschool arcade machine. Anyway. Compromise is the lesson of the day.',
-    display_image: '../static/images/toybox/display.jpg',
+    display_image: '/static/images/toybox/display.jpg',
     display_caption: 'Floor 1, 8-bit Arcade Stage',
-    carousel_image: '../static/images/toybox/carousel.jpg',
+    carousel_image: '/static/images/toybox/carousel.jpg',
     gallery: [
-      '../static/images/toybox/gallery1.jpg',
-      '../static/images/toybox/gallery2.jpg',
-      '../static/images/toybox/gallery3.jpg',
+      '/static/images/toybox/gallery1.jpg',
+      '/static/images/toybox/gallery2.jpg',
+      '/static/images/toybox/gallery3.jpg',
     ],
   },
   {
@@ -59,13 +59,13 @@ const projects = [
     about:
       "\"Foolish Vibes. Golden Times.\" is the self proclaimed motto of the social club in the heart of Stellenbosch, South Africa. Fool's Gold is one of the most popular destinations in town, one of the reasons for that being the magnificent interior design that everyone else is still trying to catch up to. Mikhail undertook installation art for Fool's gold, at it's official launch (creating the eye catching disco ball hands) as well as a few months later when a DJ area was added (courtesy of Mikhail).",
     quote: 'Booom',
-    display_image: '../static/images/fgsb/display.jpg',
+    display_image: '/static/images/fgsb/display.jpg',
     display_caption: 'Interior Decor Installation Art',
-    carousel_image: '../static/images/fgsb/carousel.jpg',
+    carousel_image: '/static/images/fgsb/carousel.jpg',
     gallery: [
-      '../static/images/fgsb/gallery1.jpg',
-      '../static/images/fgsb/gallery2.jpg',
-      '../static/images/fgsb/gallery3.jpg',
+      '/static/images/fgsb/gallery1.webp',
+      '/static/images/fgsb/gallery2.webp',
+      '/static/images/fgsb/gallery3.jpg',
     ],
   },
   {
@@ -76,13 +76,13 @@ const projects = [
     about:
       'SGL (Sexy Groovy Love) describe themselves as "a beautiful one-day musical experiece hosted at a variety of locations around the Western Cape [of South Africa]", at the centre of this edition of the experience, Mikhail led the stage design and installation, to produce something truly incredible.',
     quote: 'Mikhail says something about the SGL stage here',
-    display_image: '../static/images/sgl/display.jpg',
+    display_image: '/static/images/sgl/display.jpg',
     display_caption: 'Main Stage',
-    carousel_image: '../static/images/sgl/carousel.jpg',
+    carousel_image: '/static/images/sgl/carousel.jpg',
     gallery: [
-      '../static/images/sgl/gallery1.jpg',
-      '../static/images/sgl/gallery2.jpg',
-      '../static/images/sgl/gallery3.jpg',
+      '/static/images/sgl/gallery1.jpg',
+      '/static/images/sgl/gallery2.jpg',
+      '/static/images/sgl/gallery3.jpg',
     ],
   },
 ];
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,43 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Slider from 'react-slick';
 import projects from '../data/projects';
 
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 export default function Home() {
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    vertical: true,
+    initialSlide: 2,
+  };
   return (
-    <div className="font-raleway bg-black h-96">
-      <p className="text-offWhite">Hello</p>
-      <ul className=" mt-8">
-        {projects.map((project) => (
-          <li key={project.id}>
+    <div className="font-raleway bg-black w-screen h-screen">
+      <div id="slider">
+        <Slider {...settings}>
+          {projects.map((project) => (
             <Link
               href={`/projects/${project.id}`}
               className="font-bold text-offWhite"
+              key={project.id}
             >
-              {project.name}
+              <div className="relative w-64 h-40 mx-auto overflow-hidden rounded-lg">
+                <Image
+                  src={project.display_image}
+                  alt="display image"
+                  fill="true"
+                  className=" object-cover"
+                />
+              </div>
             </Link>
-          </li>
-        ))}
-      </ul>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
