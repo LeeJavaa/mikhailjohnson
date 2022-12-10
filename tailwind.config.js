62b4bbc6b498864f1853d1b23d8334cc97ef8920
Lee <leejavaa@gmail.com>
2022-12-10 02:40:18 +0200

    MIK-11-Navbar Complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js, jsx}', './components/**/*.{js,jsx}'],
+  content: ['./pages/**/*.{html,js,jsx}', './components/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        offWhite: '#FCFCF9',
+        light: '#D9D9D9',
+      },
+      fontFamily: {
+        raleway: "'Raleway', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
